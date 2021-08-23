2d2f5c43232c9e9ceec68c7b3ff7637175f65999
Oğuzhan <86168329+oguzhanaykuttt@users.noreply.github.com>
2021-08-24 02:10:52 +0300

    Uçaklar hareket ediyor, uçaklardan biri en alta ulaşırsa oyun bitiyor. Vurulan uçak ve vuran mermi siliniyor.

--- Savas.Library/Concrete/Ucak.cs
+++ Savas.Library/Concrete/Ucak.cs
@@ -13,9 +13,18 @@ namespace Savas.Library.Concrete
         private static readonly Random Random = new Random();
         public Ucak(Size hareketAlaniBoyutlari) : base(hareketAlaniBoyutlari)
         {
-            
+            HareketMesafesi = (int)(Height * .1);
             Left = Random.Next(hareketAlaniBoyutlari.Width - Width + 1);
 
+        }
+        public Mermi VurulduMu(List<Mermi> mermiler)
+        {
+            foreach (var mermi in mermiler)
+            {
+                var vurulduMu = mermi.Top < Bottom && mermi.Right > Left && mermi.Left < Right;
+                if (vurulduMu) return mermi;
+            }
+            return null;
         }
     }
 }
